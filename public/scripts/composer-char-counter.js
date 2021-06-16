a08777c77dec493ca9261634a77c47e93b819f8b
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-16 19:00:30 +0000

    ✍🏽 updates on the navbar position

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,36 @@
-const lengthOfChar = 140;
+//const lengthOfChar = 140;
 $(document).ready(function() {
-  $("#tweet-text").on('input', function(event) {
-    const currentLength = $(this).val().length;
-    const remainingLength = lengthOfChar - currentLength;
+  
+  $('#tweet-text').on("input", function() {
+    
+    console.log(this.value.length)
 
-    // change the color of the counter with respect to the left space.
-    if (remainingLength < 0){
-      $('.counter').css('color', 'red')
+    $(".counter")[0].innerHTML = 140 -this.value.length;
+    if ($(".counter")[0].innerHTML < 0) {
+      $(".counter").css('color', 'red');
+    }
+    else {
+      $(".counter").css('color', 'blue');
 
     }
-    else{
-      $('#tweet-text').css({'color': blue});
-
-    }
-
+  
+  
   });
 
-});
+  //})
+})
+    // const currentLength = $(this).val().length;
+    // const remainingLength = lengthOfChar - currentLength;
+    
+
+    // change the color of the counter with respect to the left space.
+  //   if (remainingLength < 0){
+  //     $('.counter').css('color', 'red')
+
+  //   }
+  //   else{
+  //     $('#tweet-text').css({'color': blue});
+
+  //   }
+
+  
