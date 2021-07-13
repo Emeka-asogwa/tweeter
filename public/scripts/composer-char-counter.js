dcb8cdfe743a217d12974483602599aedddeb2c8
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-07-13 23:39:52 +0000

    ✍🏽 implement hover on the article

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,6 @@
-//const lengthOfChar = 140;
 $(document).ready(function () {
 
   $('#tweet-text').on("input", function () {
-
-    console.log($(this))
-    //console.log($(getElementById('tweet-text')));
-
 
     $(".counter")[0].innerHTML = 140 - this.value.length;
     if ($(".counter")[0].innerHTML < 0) {
