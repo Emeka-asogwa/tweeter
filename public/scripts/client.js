75b1e67aa26adcf8ce388e4163fa6875a15c23fe
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-07-14 17:56:55 +0000

    ✍🏽 implement the timeago for tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,108 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = function () {
-  const $tweet = $(`<article class="tweet"> Hello world</article>`);
-
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
 };
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+$(document).ready(() => {
+
+
+  const createTweetElement = function (tweetData) {
+    const $tweet = $(`<article class="load-tweet">
+     <article class="top-article">
+       <div> ${tweetData.user.name}</div>
+       <div>
+       ${tweetData.user.handle}
+         <!-- <button -->
+       </div>
+     </article>
+     <div class="hello-world">${tweetData.content.text}</div>
+
+     <footer class="tweet-footer">
+       <div class="time">${timeago.format((tweetData.created_at))}</div>
+       <div class="font-awesome">
+         <i class="fa fa-solid fa-heart"></i>
+         <i class="fa fa-solid fa-flag"></i>
+         <i class="fa fa-solid fa-retweet"></i>
+       </div>
+     </footer>
+   </article>`);
+    return $tweet;
+  };
+  // const $tweet = createTweetElement();
+  // // console.log($tweet);
+  // $('#load-tweet').append($tweet);
+
+  // const $tweet = createTweetElement(tweetData);
+  // // Test / driver code (temporary)
+  // $('#tweets-container').append($tweet);
+
+
+  const renderTweets = function (tweets) {
+    for (let tweet of tweets) {
+      const $tweetValue = createTweetElement(tweet);
+
+      $('#tweets-container').append($tweetValue);
+    }
+  }
+
+
+  renderTweets(data);
+})
+
+// $(document).ready(function () {
+//   $("#tweets-container").on("submit", function (event) {
+//     event.preventDefault();
+
+//     const loadTweets = function () {
+//       $.get('/tweets')
+//         .then()
+
+//     }
+
+//   })
+// })
+
+// const validateTweet = (tweetText) => {
+//   if (!tweetText || tweetText === '\n') {
+//     return "Can't be empty";
+//   }
+//   else if (tweetText.length > 140) {
+//     return "Too many characters";
+//   }
+// };
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,15 @@ $(document).ready(function () {
 
   $('#tweet-text').on("input", function () {
 
+    /* Reference-check: An alternative way to get the value of '.counter';
+
+    # ---Traversing through tree --#
+  $('#tweet-text').on("input", function (event) {
+    const count = $(event.target).parent().next().find('.counter').val() - this.value.length;
+    console.log(count);
+  });
+*/
+
     $(".counter")[0].innerHTML = 140 - this.value.length;
     if ($(".counter")[0].innerHTML < 0) {
       $(".counter").css('color', 'red');
